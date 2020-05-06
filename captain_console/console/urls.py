24fbf2fb5d1ba64cd3b40d1b20c121c65124bf04
Pálína Kröyer <palinakroyer@Palinas-MacBook-Pro-2.local>
2020-05-06 12:44:27 +0000

    details

--- captain_console/console/urls.py
+++ captain_console/console/urls.py
@@ -3,4 +3,5 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.index, name="console-index"),
+    path('<int:id>', views.get_console_by_id, name="console_details")
 ]
--- captain_console/console/views.py
+++ captain_console/console/views.py
@@ -1,10 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from console.models import Console
 
 
 # Create your views here.
 def index(request):
+    context = {'consoles': Console.objects.all()}
+    return render(request, 'console/index.html', context)
 
-    context= {'consoles': Console.objects.all()}
-    return render(request, 'console/index.html',context)
+
+def get_console_by_id(request, id):
+    return render(request, 'console/console_details.html', {
+        'console': get_object_or_404(Console, pk=id)
+    })
