16a1807e5d1e297c1bddab28bb65fde0750648e0
Pálína Kröyer <palinakroyer@Palinas-MacBook-Pro-2.local>
2020-05-07 09:01:30 +0000

    search commit

--- captain_console/console/views.py
+++ captain_console/console/views.py
@@ -1,11 +1,23 @@
-from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
-from console.models import Console
+from django.shortcuts import render, get_object_or_404, redirect
+from django.http import JsonResponse
+
+from console.forms.console_forms import ConsoleCreateForm, ConsoleUpdateForm
+from console.models import Console, ConsoleImage
 
 
 # Create your views here.
 def index(request):
-    context = {'consoles': Console.objects.all()}
+    if 'search_filter' in request.GET:
+        search_filter = request.GET['search_filter']
+        consoles = [{
+            'id': x.id,
+            'name': x.name,
+            'description': x.description,
+            'firstImage': x.consoleimage_set.first().image
+
+        } for x in Console.objects.filter(name__icontains=search_filter)]
+        return JsonResponse({'data': consoles})
+    context = {'consoles': Console.objects.all().order_by('name')}
     return render(request, 'console/index.html', context)
 
 
@@ -13,3 +25,40 @@ def get_console_by_id(request, id):
     return render(request, 'console/console_details.html', {
         'console': get_object_or_404(Console, pk=id)
     })
+
+
+def create_console(request):
+    if request.method == 'POST':
+        form = ConsoleCreateForm(data=request.POST)
+        if form.is_valid():
+            console = form.save()
+            console_image = ConsoleImage(image=request.POST['image'], console=console)
+            console_image.save()
+            return redirect('console-index')
+    else:
+        form = ConsoleCreateForm()
+        return render(request, 'console/create_console.html', {
+            'form': form
+        })
+
+
+def delete_console(request, id):
+    console = get_object_or_404(Console, pk=id)
+    console.delete()
+    return redirect('console-index')
+
+
+def update_console(request, id):
+    instance = get_object_or_404(Console, pk=id)
+    if request.method == 'POST':
+        form = ConsoleUpdateForm(data=request.POST, instance=instance)
+        if form.is_valid():
+            form.save()
+            return redirect('console_details', id=id)
+    else:
+        form = ConsoleUpdateForm(instance=instance)
+        print(2)
+        return render(request, 'console/update_console.html', {
+            'form': form,
+            'id': id
+        })
